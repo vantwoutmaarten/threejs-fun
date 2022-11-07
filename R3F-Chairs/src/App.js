794218a9e04a873fff5cca753b1f331ae520d7f5
vantwoutmaarten <maarten@vantwout.net>
2022-11-07 20:06:21 +0100

    feat: loading a 3D model render of a chair on the screen

--- R3F-Chairs/src/App.js
+++ R3F-Chairs/src/App.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./App.scss";
 //Components
 import Header from "./components/header";
+import { Section } from "./components/section";
+
 import { Canvas } from "react-three-fiber";
-import { Html, RoundedBox } from "drei";
+
+import { Html, RoundedBox, useGLTFLoader } from "drei";
+
+const Model = () => {
+  const gltf = useGLTFLoader("/airmchairYellow.gltf", true);
+  return <primitive object={gltf.scene} dispose={null} />;
+};
 
 const HTMLContent = () => {
   return (
-    <Html fullscreen>
-      <div className="container">
-        <h1 className="title">Hello World</h1>
-      </div>
-    </Html>
+    <Section factor={1.5} offset={1}>
+      <group position={[0, 250, 0]}>
+        <mesh position={(0, 35, 0)}>
+          <Model />
+        </mesh>
+        <Html fullscreen>
+          <div className="container">
+            <h1 className="title">Hello World</h1>
+          </div>
+        </Html>
+      </group>
+    </Section>
   );
 };
 
@@ -20,7 +35,9 @@ export default function App() {
     <>
       <Header />
       <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 80 }}>
-        <HTMLContent></HTMLContent>
+        <Suspense fallback={null}>
+          <HTMLContent></HTMLContent>
+        </Suspense>
       </Canvas>
     </>
   );
