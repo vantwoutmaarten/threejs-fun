f08c2dee9559dd900b28d054111fb0c505b207ac
vantwoutmaarten <maarten@vantwout.net>
2022-12-10 16:20:48 +0100

    feat: did a fun project of converting a old js codepen project to a react component,it shows some cool text, chatGPT was super helpfull to assist in the process.

--- project2-R3F-Chairs/src/App.js
+++ project2-R3F-Chairs/src/App.js
@@ -6,27 +6,25 @@ import { Section } from "./components/section";
 
 import { Canvas } from "react-three-fiber";
 
-import { Html, RoundedBox, useGLTFLoader } from "drei";
+import { Html, useGLTFLoader } from "drei";
 
 const Model = () => {
-  const gltf = useGLTFLoader("/airmchairYellow.gltf", true);
+  const gltf = useGLTFLoader("/armchairGreen.gltf", true);
   return <primitive object={gltf.scene} dispose={null} />;
 };
 
 const HTMLContent = () => {
   return (
-    <Section factor={1.5} offset={1}>
-      <group position={[0, 250, 0]}>
-        <mesh position={(0, -35, 0)}>
-          <Model />
-        </mesh>
-        <Html fullscreen>
-          <div className="container">
-            <h1 className="title">Hello World</h1>
-          </div>
-        </Html>
-      </group>
-    </Section>
+    <group position={[0, 25, 0]}>
+      <mesh position={(0, 35, 0)}>
+        <Model />
+      </mesh>
+      <Html fullscreen>
+        <div className="container">
+          <h1 className="title">Hello World</h1>
+        </div>
+      </Html>
+    </group>
   );
 };
 
@@ -36,7 +34,7 @@ export default function App() {
       <Header />
       <Canvas colorManagement camera={{ position: [0, 0, 120], fov: 80 }}>
         <Suspense fallback={null}>
-          <HTMLContent></HTMLContent>
+          <HTMLContent />
         </Suspense>
       </Canvas>
     </>
